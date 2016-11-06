4358f8d53e328813b2cb5b5c7465758d22fd1f29
Ahmed Elsobky <0xsobky@gmail.com>
2016-11-06 22:49:10 +0200

    Add more NSE scripts

--- AutoRecon.py
+++ AutoRecon.py
@@ -19,21 +19,29 @@ def main():
     procs = []
     whois_cmd = ['whois', domain]
     dig_cmd = ['dig', '-t', 'txt', '+short', domain]
-    nmap_hosts_cmd = ['nmap', '-sn', ip_address + '/24']
     wpscan_cmd = ['wpscan', '--force', '--update', '--url', domain]
-    nmap_enum_cmd = ['nmap', '-Pn', '-sn', '--script=http-enum', domain]
-    nmap_dnsbrute_cmd = ['nmap', '-Pn', '-sn', '--script=dns-brute', domain]
-    nmap_conf_cmd = ['nmap', '-Pn', '-sn',
-                     '--script=http-config-backup', domain]
-    nmap_ports_cmd = ['nmap', '-sS', '-A', '-Pn', '-p-',
-                      '--script=http-title', domain]
-    nmap_waf_cmd = ['nmap', '-Pn', '-sn', '-sV',
-                    '--script=http-waf-fingerprint', domain]
-    cmds = {'Subdomains': nmap_dnsbrute_cmd, 'WHOIS Info': whois_cmd,
-            'Open Ports': nmap_ports_cmd, 'Active Hosts': nmap_hosts_cmd,
-            'WPScan': wpscan_cmd, 'Directories': nmap_enum_cmd,
-            'WAFs': nmap_waf_cmd, 'Config Backups': nmap_conf_cmd,
-            'TXT Records': dig_cmd}
+    nmap_hosts_cmd = ['nmap', '-sn', ip_address + '/24']
+    nmap_script_names = ('dns-brute, hostmap-ip2hosts, banner,'
+                         'http-robots.txt, http-crossdomainxml, http-enum,'
+                         'http-config-backup, http-devframework,'
+                         'http-waf-fingerprint, http-sitemap-generator,'
+                         'http-xssed, http-shellshock, ftp-anon, ssl-cert,'
+                         'ssl-poodle, ssl-heartbleed, ssl-enum-ciphers')
+    nmap_full_cmd = ['nmap', '-sV', '-sS', '-A', '-Pn', '--script',
+                     nmap_script_names, domain]
+    cmds = {'TXT Records': dig_cmd, 'WHOIS Info': whois_cmd,
+            'Nmap Results': nmap_full_cmd, 'Active Hosts': nmap_hosts_cmd,
+            'WPScan': wpscan_cmd}
+
+    def handle_proc(proc):
+        """ handles subprocesses outputs """
+        separator = '=================='
+        output = ''.join(proc.stdout.readlines())
+        print proc.title
+        print separator
+        print output.strip()
+        print separator + '\n'
+        procs.remove(proc)
 
     for title, cmd in cmds.items():
         try:
@@ -43,16 +51,6 @@ def main():
         except OSError:
             print '%s >> Dependency error occurred!\n' % title
 
-    def handle_proc(proc):
-        """ handles active subprocesses """
-        separator = '=================='
-        output = ''.join(proc.stdout.readlines())
-        print proc.title
-        print separator
-        print output.strip()
-        print separator + '\n'
-        procs.remove(proc)
-
     while True:
         for proc in procs:
             retcode = proc.poll()
@@ -60,7 +58,6 @@ def main():
                 handle_proc(proc)
             else:
                 continue
-
         if len(procs) == 0:
             break
         else:
@@ -69,4 +66,3 @@ def main():
 if __name__ == '__main__':
     print "This is gonna take quite a while; you better go make some coffee!\n"
     main()
-
