466d661111ecb99e1ac87a5e2634c9e4109ac5e1
Ahmed Elsobky <0xsobky@gmail.com>
2016-12-19 01:46:50 +0200

    Rename NSE scripts

--- AutoRecon.py
+++ AutoRecon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Automates the reconnaissance process of web hacking.
+Automate the reconnaissance process of web hacking.
 """
 import sys
 import socket
@@ -9,32 +9,34 @@ import subprocess
 from time import sleep
 
 def main():
-    """ Executes main code """
-    domain = sys.argv[1]
+    """Execute main code."""
     try:
+        domain = sys.argv[1]
         ip_address = socket.gethostbyname(domain)
+    except IndexError:
+        print 'Error: Domain name not specified.'
+        sys.exit(1)
     except socket.gaierror:
-        print 'Error: Domain name cannot be resolved!'
+        print 'Error: Domain name cannot be resolved.'
         raise
     procs = []
     whois_cmd = ['whois', domain]
     dig_cmd = ['dig', '-t', 'txt', '+short', domain]
     wpscan_cmd = ['wpscan', '--force', '--update', '--url', domain]
     nmap_hosts_cmd = ['nmap', '-sn', ip_address + '/24']
-    nmap_script_names = ('dns-brute, hostmap-ip2hosts, banner, http-headers,'
-                         'http-robots.txt, http-crossdomainxml, http-enum,'
-                         'http-config-backup, http-devframework, http-methods,'
-                         'http-waf-fingerprint, http-sitemap-generator,'
-                         'http-xssed, http-shellshock, ftp-anon, ssl-cert,'
-                         'ssl-poodle, ssl-heartbleed, ssl-enum-ciphers')
+    nmap_script_names = ('banner, dns-brute, ftp-anon, hostmap-ip2hosts,'
+                         'http-config-backup, http-cross*, http-devframework,'
+                         'http-enum, http-headers, http-methods, http-robots.txt,'
+                         'http-shellshock, http-sitemap-generator, http-waf-fingerprint,'
+                         'http-xssed, ssl-cert, ssl-enum-ciphers, ssl-heartbleed, ssl-poodle')
     nmap_full_cmd = ['nmap', '-sV', '-sS', '-A', '-Pn', '--script',
                      nmap_script_names, domain]
     cmds = {'TXT Records': dig_cmd, 'WHOIS Info': whois_cmd,
-            'Nmap Results': nmap_full_cmd, 'Active Hosts': nmap_hosts_cmd,
+            'Active Hosts': nmap_hosts_cmd, 'Nmap Results': nmap_full_cmd,
             'WPScan': wpscan_cmd}
 
     def handle_proc(proc):
-        """ handles subprocesses outputs """
+        """Handle subprocesses outputs."""
         separator = '=================='
         output = ''.join(proc.stdout.readlines())
         print proc.title
@@ -45,11 +47,11 @@ def main():
 
     for title, cmd in cmds.items():
         try:
-            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             proc.title = title
             procs.append(proc)
         except OSError:
-            print '%s >> Dependency error occurred!\n' % title
+            print '%s >> Dependency error occurred.\n' % title
 
     while True:
         for proc in procs:
@@ -64,5 +66,5 @@ def main():
             sleep(1)
 
 if __name__ == '__main__':
-    print "This is gonna take quite a while; you better go make some coffee!\n"
+    print 'This is gonna take quite a while; you better go make some coffee!\n'
     main()
